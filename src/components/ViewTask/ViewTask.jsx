6526a459aba562641a59429a4bdce59d3d381276
misraag <jaiofficial50@gmail.com>
2024-06-10 11:23:33 +0530

    showing clicked task from viewtask component

--- src/components/ViewTask/ViewTask.jsx
+++ src/components/ViewTask/ViewTask.jsx
@@ -1,7 +1,17 @@
-export default function ViewTask({listOfProjects, selectedProject, ...props}) {
-    return (
-        <div className="flex h-screen my-8 flex gap-8">
-            <h1>Hello World</h1>
-        </div>
-    )
+export default function ViewTask({
+  listOfProjects,
+  selectedProject,
+  ...props
+}) {
+  const project = listOfProjects.find(
+    (project) => project.title === selectedProject
+  );
+
+  return (
+    <div className="flex h-screen my-8 flex gap-8">
+      <h2 className="text-xl font-bold">{project.title}</h2>
+      <p className="text-gray-600">{project.description}</p>
+      <p className="text-gray-500">{project.dueDate}</p>
+    </div>
+  );
 }
